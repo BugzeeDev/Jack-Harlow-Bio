9f1ed9eeabbacdb16445027e6fcea606c8716d14
BugzeeDev <sylva.irving91@gmail.com>
2024-04-29 21:35:10 -0700

    reformatted and cleaned up code

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 <script>
 //Defines a function to be called later  -- my signature
-  function bdAuth() {
-    console.log("------------------------")
+    function bdAuth() {
+        console.log("------------------------")
     console.log("-- Coded by BugzeeDev --")
     console.log("------------------------")
   }
 
-//Calls the bdAuth function
-  bdAuth()
+    //Calls the bdAuth function
+    bdAuth()
 
-//defines a variable and then logs that vairable to the dev console
-const testJavascript = "Javascript code is running..."
-console.log(testJavascript)
+    //defines a variable and then logs that vairable to the dev console
+    const testJavascript = "Javascript code is running..."
+    console.log(testJavascript)
 </script>
